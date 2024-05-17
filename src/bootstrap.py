0510126255917db0fe5194d2e8726892944007f6
darmangerd <david.darmanger@protonmail.com>
2024-05-17 15:23:43 +0200

    Log the size of the communication

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -34,20 +34,21 @@ parser.add_argument("--iid", type=int, default=1)
 parser.add_argument("--seed", type=int, default=1)
 args = parser.parse_args()
 
+
 def verify_imports(imports_options: Dict[str, Any], chosen: str) -> None:
     if chosen.lower() not in imports_options:
         raise ValueError(
-            f"Option \"{args.dataset}\" not available. Choose from {imports_options.keys()}"
+            f'Option "{args.dataset}" not available. Choose from {imports_options.keys()}'
         )
+
 
 def weights_init(m: nn.Module) -> None:
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find("Conv") != -1:
         m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
+    elif classname.find("BatchNorm") != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
-
 
 if __name__ == "__main__":
     os.environ["MASTER_ADDR"] = args.master_addr
@@ -57,57 +58,13 @@ if __name__ == "__main__":
     os.environ["GLOO_SOCKET_IFNAME"] = "en0"
     os.environ["USE_CUDA"] = "0"
 
-    os.environ['GLOO_LOG_LEVEL'] = 'DEBUG'
-    os.environ["TORCH_CPP_LOG_LEVEL"]="INFO"
-    os.environ[
-        "TORCH_DISTRIBUTED_DEBUG"
-    ] = "DETAIL"
-    os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
+    os.environ["GLOO_LOG_LEVEL"] = "DEBUG"
+    os.environ["TORCH_CPP_LOG_LEVEL"] = "INFO"
+    os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
+    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
     log_folder: Path = Path("logs")
     log_folder.mkdir(parents=True, exist_ok=True)
-
-    from dataloaders.Cifar10Partitioner import Cifar10Partitioner, cifar10_shape
-    from dataloaders.MnistPartitioner import MnistPartitioner, mnist_shape
-    from dataloaders.CelebaPartitioner import CelebaPartitioner, celeba_shape
-
-    available_datasets: Dict[str, Tuple[DataPartitioner, int]] = {
-        "cifar": (Cifar10Partitioner, cifar10_shape),
-        "mnist": (MnistPartitioner, mnist_shape),
-        "celeba": (CelebaPartitioner, celeba_shape),
-    }
-    verify_imports(available_datasets, args.dataset)
-    dataset: DataPartitioner = available_datasets[args.dataset][0](args.world_size - 1, args.rank)
-    image_shape = available_datasets[args.dataset][1]
-    dataset.load_data()
-
-    from models.CifarDiscriminator import CifarDiscriminator
-    from models.MnistDiscriminator import MnistDiscriminator
-    from models.CelebaDiscriminator import CelebaDiscriminator
-
-    available_discriminators: Dict[str, torch.nn.Module] = {
-        "cifar": CifarDiscriminator,
-        "mnist": MnistDiscriminator,
-        "celeba": CelebaDiscriminator,
-    }
-    verify_imports(available_discriminators, args.model)
-    discriminator: torch.nn.Module = available_discriminators[args.model]
-
-    from models.CifarGenerator import CifarGenerator, cifar_z_dim
-    from models.MnistGenerator import MnistGenerator, mnist_z_dim
-    from models.CelebaGenerator import CelebaGenerator, celeba_z_dim
-
-    available_generators: Dict[str, torch.nn.Module] = {
-        "cifar": (CifarGenerator, cifar_z_dim),
-        "mnist": (MnistGenerator, mnist_z_dim),
-        "celeba": (CelebaGenerator, celeba_z_dim),
-    }
-    verify_imports(available_generators, args.model)
-    generator: torch.nn.Module = available_generators[args.model][0]
-    z_dim = available_generators[args.model][1]
-
-    from actors.server import server
-    from actors.worker import worker
 
     seed_shifted = args.seed + args.rank
     np.random.seed(seed_shifted)
@@ -118,6 +75,53 @@ if __name__ == "__main__":
     torch.cuda.manual_seed_all(seed_shifted)
     torch.backends.cudnn.deterministic = True
 
+    from dataloaders.Cifar10Partitioner import Cifar10Partitioner, cifar10_shape
+    from dataloaders.MnistPartitioner import MnistPartitioner, mnist_shape
+    from dataloaders.CelebaPartitioner import CelebaPartitioner, celeba_shape
+
+    from models.CifarDiscriminator import CifarDiscriminator
+    from models.MnistDiscriminator import MnistDiscriminator
+    from models.CelebaDiscriminator import CelebaDiscriminator
+
+    from models.CifarGenerator import CifarGenerator, cifar_z_dim
+    from models.MnistGenerator import MnistGenerator, mnist_z_dim
+    from models.CelebaGenerator import CelebaGenerator, celeba_z_dim
+
+    from actors.server import server
+    from actors.worker import worker
+
+    available_datasets: Dict[str, Tuple[DataPartitioner, int]] = {
+        "cifar": (Cifar10Partitioner, cifar10_shape),
+        "mnist": (MnistPartitioner, mnist_shape),
+        "celeba": (CelebaPartitioner, celeba_shape),
+    }
+
+    available_discriminators: Dict[str, torch.nn.Module] = {
+        "cifar": CifarDiscriminator,
+        "mnist": MnistDiscriminator,
+        "celeba": CelebaDiscriminator,
+    }
+
+    available_generators: Dict[str, torch.nn.Module] = {
+        "cifar": (CifarGenerator, cifar_z_dim),
+        "mnist": (MnistGenerator, mnist_z_dim),
+        "celeba": (CelebaGenerator, celeba_z_dim),
+    }
+
+    verify_imports(available_datasets, args.dataset)
+    verify_imports(available_discriminators, args.model)
+    verify_imports(available_generators, args.model)
+
+    dataset: DataPartitioner = available_datasets[args.dataset][0](
+        args.world_size - 1, args.rank
+    )
+    image_shape = available_datasets[args.dataset][1]
+    dataset.load_data()
+
+    discriminator: torch.nn.Module = available_discriminators[args.model]
+
+    generator: torch.nn.Module = available_generators[args.model][0]
+    z_dim = available_generators[args.model][1]
     # If the rank is greater than 0, we are a worker
     if args.rank > 0:
         # Initialize dataset with world size-1 because the server should not count as a worker
@@ -163,6 +167,6 @@ if __name__ == "__main__":
             z_dim=z_dim,
             log_interval=args.log_interval,
             log_folder=log_folder,
-            iid=args.iid==1,
+            iid=args.iid == 1,
             dataset_name=args.dataset,
         )
